600c926b32df2e0500cd1b98808522bba8b9bb48
haozi23333 <haozi23333@gmail.com>
2015-07-27 13:32:58 +0800

    简单的迷宫canvas

--- a.js
+++ a.js
@@ -1,32 +1,32 @@
 var express = require('express')
 var app= express();
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'bb_liulan',
-    port: 3306
-});
-conn.connect();
+//var mysql = require('mysql');
+//var conn = mysql.createConnection({
+//    host: 'localhost',
+//    user: 'root',
+//    password: 'admin',
+//    database: 'bb_liulan',
+//    port: 3306
+//});
+//conn.connect();
 
 app.use(express.static(__dirname + '/public'));
 
 
 
 app.listen(80,function(){
-    console.log("server is start")
+    console.log("server is start");
 })
 
 
 app.get("/bb",function(req,res){
-    conn.query("call bb_ip('"+ip(req)+"');")
+    //conn.query("call bb_ip('"+ip(req)+"');")
     res.redirect("bb.html");
 
 });
 
 app.get("/cy",function(req,res){
-    conn.query("call cy_ip('"+ip(req)+"');")
+    //conn.query("call cy_ip('"+ip(req)+"');")
     res.redirect("cy.html")
 })
 
